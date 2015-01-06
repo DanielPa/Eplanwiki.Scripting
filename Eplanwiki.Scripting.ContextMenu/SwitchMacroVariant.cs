e071637e6f42b3daa6ca1ea8701777c399122641
DanielPa <papp.technik@gmail.com>
2015-01-06 21:04:05 +0100

    Tidied up and added project settings check

--- Eplanwiki.Scripting.ContextMenu/SwitchMacroVariant.cs
+++ Eplanwiki.Scripting.ContextMenu/SwitchMacroVariant.cs
@@ -1,8 +1,15 @@
-﻿using Eplan.EplApi.ApplicationFramework;
+﻿/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+using Eplan.EplApi.ApplicationFramework;
+using Eplan.EplApi.Base;
 using Eplan.EplApi.Scripting;
 using System;
 using System.Diagnostics;
+using System.IO;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace Eplanwiki.Scripting.ContextMenu
 {
@@ -44,30 +51,30 @@ namespace Eplanwiki.Scripting.ContextMenu
             // 
             this.comboBoxVariant.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxVariant.FormattingEnabled = true;
-            this.comboBoxVariant.Location = new System.Drawing.Point(97, 8);
-            this.comboBoxVariant.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBoxVariant.Location = new System.Drawing.Point(121, 10);
+            this.comboBoxVariant.Margin = new System.Windows.Forms.Padding(2);
             this.comboBoxVariant.Name = "comboBoxVariant";
-            this.comboBoxVariant.Size = new System.Drawing.Size(82, 21);
+            this.comboBoxVariant.Size = new System.Drawing.Size(102, 24);
             this.comboBoxVariant.TabIndex = 0;
             this.comboBoxVariant.SelectedIndexChanged += new System.EventHandler(this.comboBoxVariant_SelectedIndexChanged);
             // 
             // labelVariant
             // 
             this.labelVariant.AutoSize = true;
-            this.labelVariant.Location = new System.Drawing.Point(9, 12);
+            this.labelVariant.Location = new System.Drawing.Point(11, 15);
             this.labelVariant.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelVariant.Name = "labelVariant";
-            this.labelVariant.Size = new System.Drawing.Size(40, 13);
+            this.labelVariant.Size = new System.Drawing.Size(53, 17);
             this.labelVariant.TabIndex = 1;
             this.labelVariant.Text = "Variant";
             // 
             // buttonOk
             // 
             this.buttonOk.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.buttonOk.Location = new System.Drawing.Point(11, 60);
-            this.buttonOk.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonOk.Location = new System.Drawing.Point(14, 76);
+            this.buttonOk.Margin = new System.Windows.Forms.Padding(2);
             this.buttonOk.Name = "buttonOk";
-            this.buttonOk.Size = new System.Drawing.Size(81, 24);
+            this.buttonOk.Size = new System.Drawing.Size(101, 31);
             this.buttonOk.TabIndex = 2;
             this.buttonOk.Text = "OK";
             this.buttonOk.UseVisualStyleBackColor = true;
@@ -75,10 +82,10 @@ namespace Eplanwiki.Scripting.ContextMenu
             // buttonCancel
             // 
             this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(97, 60);
-            this.buttonCancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonCancel.Location = new System.Drawing.Point(121, 76);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(2);
             this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(81, 24);
+            this.buttonCancel.Size = new System.Drawing.Size(101, 31);
             this.buttonCancel.TabIndex = 3;
             this.buttonCancel.Text = "Cancel";
             this.buttonCancel.UseVisualStyleBackColor = true;
@@ -87,17 +94,18 @@ namespace Eplanwiki.Scripting.ContextMenu
             // VarinatSelectionForm
             // 
             this.AcceptButton = this.buttonOk;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
             this.CancelButton = this.buttonCancel;
-            this.ClientSize = new System.Drawing.Size(185, 95);
+            this.ClientSize = new System.Drawing.Size(231, 121);
             this.ControlBox = false;
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonOk);
             this.Controls.Add(this.labelVariant);
             this.Controls.Add(this.comboBoxVariant);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "VarinatSelectionForm";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
@@ -117,10 +125,53 @@ namespace Eplanwiki.Scripting.ContextMenu
     }
 
     /// <summary>
+    /// Contains static text for usercontrolls
+    /// Uncoment/comment the section you want/don't want before loading the script
+    /// </summary>
+    public static class TextHelper
+    {
+        #region English
+        public static string MESSAGE_BOX_TEXT_PROJECT_SETTING = "The project setting 'Insert macrobox' is not set to true.\n" +
+                    "The macrobox would get lost this way. If your Electric P8 version is 2.3 or higher, the setting could be done automatically.\n\n" +
+                    "Should the project setting 'Insert macrobox' set to true?";
+        public static string MESSAGE_BOX_TITEL_PROJECT_SETTING = "Project setting 'Insert macrobox'";
+        public static string CONTEXT_MENU_ITEM_TEXT = "Switch Variant...";
+        public static string VARIANT = "Variant";
+        #endregion
+
+        #region German
+        //public static string MESSAGE_BOX_TEXT_PROJECT_SETTING = "Die Projekteinstellung 'Makrokästen mit einfügen' ist nicht gesetzt.\n" +
+        //            "Auf diese Weise würde der Makrokasten verloren gehen. Ist Ihre Electric P8 Version 2.3 oder höher, kann die Einstellung automatisch gesetzt werden.\n\n" +
+        //            "Soll die Projekteinstellung 'Makrokästen mit einfügen' gesetzt werden?";
+        //public static string MESSAGE_BOX_TITEL_PROJECT_SETTING = "Projekteinstellung 'Makrokästen mit einfügen'";
+        //public static string CONTEXT_MENU_ITEM_TEXT = "Variante umschalten...";
+        //public static string VARIANT = "Variante";
+        #endregion
+    }
+
+    /// <summary>
     /// Main class with menu entry and action
     /// </summary>
     public class SwitchMacroVariant
-    {
+    {   
+        /// <summary>
+        /// The on register script.
+        /// </summary>
+        [DeclareRegister]
+        public void OnRegisterScript()
+        {
+            AddVarinatSelectionMenuItem();
+        }
+
+        /// <summary>
+        /// The on un register script.
+        /// </summary>
+        [DeclareUnregister]
+        public void OnUnRegisterScript()
+        {
+            RemoveVarinatSelectionMenuItem();
+        } 
+   
         /// <summary>
         /// Action gets started from contextmenu in GED
         /// </summary>
@@ -142,35 +193,113 @@ namespace Eplanwiki.Scripting.ContextMenu
                     UpdateMacro();
                 }
             }
+            
+            else
+            {
+               DialogResult dResult = MessageBox.Show(TextHelper.MESSAGE_BOX_TEXT_PROJECT_SETTING, TextHelper.MESSAGE_BOX_TITEL_PROJECT_SETTING, MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+               switch (dResult)
+               {                                    
+                   case DialogResult.No:                       
+                       break;                   
+                   case DialogResult.Yes:
+                       SetProjectSetting("MacrosGui.InsertMacrobox", "1");
+                       VarinatSelectionAction();
+                       break;
+                   default:
+                       break;
+               }
+            }
         }
 
         /// <summary>
         /// Adds the menueitem to contextmenu in GED
-        /// </summary>
-        [DeclareMenu]
-        public void VarinatSelectionMenuItem()
+        /// </summary>        
+        private void AddVarinatSelectionMenuItem()
         {
             Eplan.EplApi.Gui.ContextMenu oMenu = new Eplan.EplApi.Gui.ContextMenu();
             Eplan.EplApi.Gui.ContextMenuLocation oLocation = new Eplan.EplApi.Gui.ContextMenuLocation("Editor", "Ged");
-            oMenu.AddMenuItem(oLocation, "Switch Variant", "VarinatSelectionAction", true, false);
+            oMenu.AddMenuItem(oLocation, TextHelper.CONTEXT_MENU_ITEM_TEXT, "VarinatSelectionAction", true, false);            
         }
 
-        //TODO
+        /// <summary>
+        /// Adds the menueitem to contextmenu in GED
+        /// </summary>        
+        private void RemoveVarinatSelectionMenuItem()
+        {
+            Eplan.EplApi.Gui.ContextMenu oMenu = new Eplan.EplApi.Gui.ContextMenu();
+            Eplan.EplApi.Gui.ContextMenuLocation oLocation = new Eplan.EplApi.Gui.ContextMenuLocation("Editor", "Ged");
+            oMenu.RemoveMenuItem(oLocation, TextHelper.CONTEXT_MENU_ITEM_TEXT, "VarinatSelectionAction", true, false);
+        }
+
         /// <summary>
         /// Should check if the "/Settings/CAT/MOD/Setting:InsertMacrobox" has Val = 1
         /// Is there a way to read project settings without API? Maybe with export
+        /// YES THERE IS A WAY!!! THANKS TO FrankS
         /// </summary>
         /// <returns></returns>
         private bool CheckPlaceMacroBoxSetting()
         {
-            return true;
+            string node = "MacrosGui";
+            string setting = "/Settings/CAT/MOD/Setting[@name='InsertMacrobox']/Val";
+            string settingTemp = GetProjectSetting(node, setting);
+            if (settingTemp.Equals("1"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
         }
+        
+        /// <summary>
+        /// Exports a node from projectsettings to a temp file and reads the setting value
+        /// </summary>
+        /// <param name="node">´'name'-attribute value of the MOD element to export</param>
+        /// <param name="setting">XPath of the setting to read</param>
+        /// <returns>value of the setting as string</returns>
+        private string GetProjectSetting(string node, string setting)
+        {
+            try
+            {
+                string projectPath = PathMap.SubstitutePath("$(PROJECTPATH)");
+                string settingsTempFile = Path.Combine(System.Environment.GetEnvironmentVariable("TEMP"), "GetProjectSetting_temp.xml");
+                new CommandLineInterpreter().Execute("XSettingsExport /XmlFile:\"" + settingsTempFile + "\" /prj:\"" + projectPath + "\" /Node:" + node);
+                var xmlDocument = new XmlDocument();
+                xmlDocument.Load(settingsTempFile);
+                var settingNode = xmlDocument.DocumentElement.SelectSingleNode(setting);
+                var projectSetting = settingNode.InnerText;
+                File.Delete(settingsTempFile);
+                return projectSetting;
+            }
+            catch (BaseException exc)
+            {
+                String strMessage = exc.Message;
+                System.Windows.Forms.MessageBox.Show("Exception: " + strMessage);
+                return string.Empty;
+            }
+        }
+        
+        /// <summary>
+        /// The action XAfActionSettingProject is only available in version 2.3 and higher
+        /// </summary>
+        /// <param name="setting"></param>
+        /// <param name="value"></param>
+        private void SetProjectSetting(string setting, string value)
+        {
+            CommandLineInterpreter interpreter = new CommandLineInterpreter();
+            ActionCallingContext XAfActionSettingProjectContext = new ActionCallingContext();
+            XAfActionSettingProjectContext.AddParameter("set", setting);
+            XAfActionSettingProjectContext.AddParameter("value", value);
+            interpreter.Execute("XAfActionSettingProject", XAfActionSettingProjectContext);
+        }               
 
         /// <summary>
         /// Sets the variant of the selected macrobox
         /// </summary>
         /// <param name="variant">0-15 = A-P</param>
-        public void SetMacroboxVariant(int variant)
+        private void SetMacroboxVariant(int variant)
         {
             CommandLineInterpreter interpreter = new CommandLineInterpreter();
             ActionCallingContext XEsSetPropertyActionContext = new ActionCallingContext();
@@ -183,10 +312,11 @@ namespace Eplanwiki.Scripting.ContextMenu
         /// <summary>
         /// Macrobox has to be selected in graphical editor
         /// </summary>
-        public void UpdateMacro()
+        private void UpdateMacro()
         {
             CommandLineInterpreter interpreter = new CommandLineInterpreter();
             interpreter.Execute("XGedUpdateMacroAction");
+            interpreter.Execute("XMSelectObjectsInsideMacroBoxAction");
         }
     }
 
@@ -206,6 +336,8 @@ namespace Eplanwiki.Scripting.ContextMenu
         public VarinatSelectionForm()
         {
             InitializeComponent();
+            this.labelVariant.Text = TextHelper.VARIANT;
+            this.Text = TextHelper.VARIANT;
             this.comboBoxVariant.Items.AddRange(VariantItems());
             this.comboBoxVariant.SelectedIndex = 0;
         }
