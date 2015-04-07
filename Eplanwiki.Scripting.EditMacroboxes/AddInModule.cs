52005c93cfa4471ced26abb7b7adaa402ce010d9
DanielPa <papp.technik@gmail.com>
2015-04-08 00:39:28 +0200

    Classdiagramm

---
+++ Eplanwiki.Scripting.EditMacroboxes/AddInModule.cs
@@ -0,0 +1,38 @@
+﻿using Eplan.EplApi.ApplicationFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eplanwiki.Scripting.EditMacroboxes
+{
+    public class AddInModule : IEplAddIn
+    {
+        public bool OnExit()
+        {
+            return true;
+        }
+
+        public bool OnInit()
+        {
+            return true;
+        }
+
+        public bool OnInitGui()
+        {
+            return true;
+        }
+
+        public bool OnRegister(ref bool bLoadOnStart)
+        {
+            bLoadOnStart = true;
+            return true;
+        }
+
+        public bool OnUnregister()
+        {
+            return true;
+        }
+    }
+}
